b155806d86869503b38edcfad3cc6203f20de90e
Jeff Remer <jeff@strava.com>
2012-12-04 15:13:13 -0800

    Initial gb gem setup and CLI implementation

---
+++ lib/gb/cli.rb
@@ -0,0 +1,58 @@
+require 'ostruct'
+require 'thor'
+require 'yaml'
+
+module GB
+  class CLI < Thor
+    include Thor::Actions
+    
+    desc "fix [NUMBER] [SHORT_DESCRIPTION]", "Creates a bug fix branch"
+    def fix(number, *name)
+      run "git checkout -b #{committer}/#{issue_prefix}-#{number}/#{name.join(' ').branchify}"
+    end
+    
+    desc "feature [SHORT_DESCRIPTION]", "Creates a feature branch"        
+    def feature(*name)
+      run "git checkout -b #{committer}/#{name.join(' ').branchify}"
+    end
+    
+    desc "install", "Runs the first time configuration"
+    def install
+      configure
+    end
+    
+    private
+
+    @@FILE_NAME = '.gbconfig'
+    def config
+      @config ||= OpenStruct.new(YAML.load(File.open(@@FILE_NAME)))
+    rescue
+      say "gb isn't configured yet, configuring...", :red
+      configure
+    end
+    
+    def configure
+      options = {}
+      options[:issue_prefix] = ask "Issue prefix?"
+      options[:committer] = ask "Committer initials?"      
+      File.open(@@FILE_NAME, 'w') do |f|
+        f.write(options.to_yaml)
+      end
+      config
+    end
+    
+    def committer
+      config.committer
+    end
+    
+    def issue_prefix
+      config.issue_prefix
+    end
+  end  
+end
+
+class String
+  def branchify
+    downcase.gsub(' ', '-')
+  end
+end
